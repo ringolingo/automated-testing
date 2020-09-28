37ea820206649682a03e6f06792dff01be6af7d9
Ringo Alcock <ringo.alcock@gmail.com>
2020-09-28 14:25:41 -0700

    raise ArgumentError for invalid cards

--- lib/card.rb
+++ lib/card.rb
@@ -5,9 +5,17 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+
+    unless (1..13).include?(value)
+      raise ArgumentError.new("improper card value: #{value}")
+    end
+
+    unless [:diamonds, :hearts, :spades, :clubs].include?(suit)
+      raise ArgumentError.new("improper card suit: #{suit}")
+    end
+
     @value = value
     @suit = suit
-    
 
   end
 
