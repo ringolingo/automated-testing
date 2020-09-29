1222b8187fd1a7ac47f512c5bfe7d573b6d4c641
Ringo Alcock <ringo.alcock@gmail.com>
2020-09-28 20:45:31 -0700

    define initialize, draw, shuffle, count methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,15 +5,29 @@ require_relative 'card'
 
 class Deck
 
-  def initialize
+  attr_reader :cards
 
+  def initialize
+    @cards = []
+
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 
   def draw
-    # returns a card
+    raise ArgumentError.new("you cannot draw when the deck is empty") if @cards.length == 0
+    return @cards.shift
   end
 
   def shuffle
-    # shuffles the deck
+    return @cards.shuffle!
+  end
+
+  def count
+    return @cards.length
   end
 end
+
